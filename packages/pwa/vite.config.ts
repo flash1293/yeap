87b3faea2e272dd80803fd4111fa5e586cb62279
Joe Reuter <flash1293@gmail.com>
2026-05-16 20:26:32 +0200

    fix: add /chat to service worker navigate fallback denylist

--- packages/pwa/vite.config.ts
+++ packages/pwa/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
-        navigateFallbackDenylist: [/^\/llmapi/],
+        navigateFallbackDenylist: [/^\/llmapi/, /^\/chat/],
       },
       manifest: {
         name: 'YEAP',
